0f690ce1f513987747839602672bf8bc499ca65f
Flavia Arsuffi <flavia.arsuffi@gmail.com>
2024-01-22 16:58:47 -0300

    search bar ui structure

---
+++ AirDnd/Core/Explore/Components/SearchAndFilterBar.swift
@@ -0,0 +1,47 @@
+//
+//  SearchAndFilterBar.swift
+//  AirDnd
+//
+//  Created by Flavia Arsuffi on 22/01/24.
+//
+
+import SwiftUI
+
+struct SearchAndFilterBar: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+            
+            VStack(alignment: .leading, spacing: 2) {
+                Text("Where to?")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                
+                Text("Anywhere - Any week - Add guests")
+                    .font(.caption2)
+                    .foregroundStyle(.gray)
+            }
+            Spacer()
+            
+            Button(action: {}, label: {
+                Image(systemName: "line.3.horizontal.decrease.circle")
+                    .foregroundStyle(.black)
+            })
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 10)
+        .overlay {
+            Capsule()
+                .stroke(lineWidth: 0.5)
+                .foregroundStyle(Color(.systemGray4))
+                .shadow(color: .black.opacity(0.4), radius: 2)
+        }
+        .padding()
+    }
+}
+
+struct SearchAndFilterBar_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchAndFilterBar()
+    }
+}
--- AirDnd/Core/Explore/ExploreView.swift
+++ AirDnd/Core/Explore/ExploreView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ExploreView: View {
     var body: some View {
         NavigationStack {
+            VStack {
+                SearchAndFilterBar()
+            }
             ScrollView {
                 LazyVStack(spacing: 32) {
                     ForEach(0 ... 10, id: \.self) { listing in
