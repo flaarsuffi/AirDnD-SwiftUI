8f2ca7684249cf0c358eb75f44255d165ae3c413
Flavia Arsuffi <flavia.arsuffi@gmail.com>
2024-01-22 10:30:10 -0300

    Initial Commit

---
+++ AirDnd/AirDndApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirDndApp.swift
+//  AirDnd
+//
+//  Created by Flavia Arsuffi on 22/01/24.
+//
+
+import SwiftUI
+
+@main
+struct AirDndApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
