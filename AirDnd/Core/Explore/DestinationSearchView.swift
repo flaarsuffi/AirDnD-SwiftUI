68453b23c03a08dd293c6fd7fdf699b352dc0b6a
Flavia Arsuffi <flavia.arsuffi@gmail.com>
2024-02-05 14:30:32 -0300

    Create searchview ui structure

---
+++ AirDnd/Core/Explore/DestinationSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  DestinationSearchView.swift
+//  AirDnd
+//
+//  Created by Flavia Arsuffi on 01/02/24.
+//
+
+import SwiftUI
+
+struct DestinationSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DestinationSearchView()
+}
